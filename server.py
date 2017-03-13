313a1bb5c42002def0d7abdd523cc04bf9c5a346
Setceck <klemici@outlook.com>
2017-03-13 17:00:10 +0100

    Bot

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from oggetti import Automobile
 
 
@@ -8,11 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
+'''
 @app.route('/saluta/<nome>')
 def saluta(nome):
     return 'Ciao %s' % nome
-
+'''
 @app.route('/somma/<int:a>/<int:b>')
 def somma(a, b):
     somma = a + b    
@@ -22,5 +22,28 @@ def somma(a, b):
 def crea_automobile(nome, anno, marca, consumo):
     auto = Automobile(nome, anno, marca, consumo)
     return auto.nome
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/saluta', methods=['POST', 'GET'])
+def saluta():
+    name = request.form['name']
+    return render_template('saluta.html', name=name)
+
+@app.route('/nuovaauto')
+def nuovaauto():
+    return render_template('nuovaauto.html')
+
+@app.route('/salvaauto', methods=['POST'])
+def salvaauto():
+    nome = request.form['name']
+    anno = request.form['year']
+    marca = request.form['brand']
+    consumo = request.form['consumption']
+
+    auto = Automobile(nome, anno, marca, consumo)
+    return render_template('nuovaauto.html', name=auto.nome)
 
 app.run()
